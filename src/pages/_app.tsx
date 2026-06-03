2be2b998af61fbacb766698c6a49b060e6ee5ced
royg4467-2062 <royg4467@gmail.com>
2026-06-03 05:01:46 +0000

    Add google_analytics integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import Script from 'next/script'
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@/contexts/ThemeProvider";
 import { Toaster } from "@/components/ui/toaster";
@@ -31,7 +32,25 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <ThemeProvider>
-      <Component {...pageProps} />
+      {/* Google Analytics 4 */}
+<Script
+    id="google-analytics-4"
+    src="https://www.googletagmanager.com/gtag/js?id=G-9ZG1992263"
+    strategy="afterInteractive"
+/>
+              <Script
+                  id="google-analytics-4-inline"
+                  strategy="afterInteractive"
+                  dangerouslySetInnerHTML={{
+                      __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-9ZG1992263');`
+                  }}
+              />
+{/* End Google Analytics 4 */}
+<Component {...pageProps} />
       <Toaster />
       <InstallPrompt />
     </ThemeProvider>
